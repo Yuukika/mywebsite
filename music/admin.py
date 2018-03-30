15007ad7492d3e94e99255975c48c1257ef8e212
FormulaText <758774239@qq.com>
2018-03-30 19:12:41 +0800

    modify

--- music/admin.py
+++ music/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Song
+from .models import Song,Song_like,Play_list
 
 
 admin.site.register(Song)
+admin.site.register(Song_like)
+admin.site.register(Play_list)
